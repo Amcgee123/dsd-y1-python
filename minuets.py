0ca3f46bd56106832b359bb0efa59b42e37608bf
Amcgee123 <austinmcgee33@gmail.com>
2025-11-24 11:01:30 +0000

    cng

--- minuets.py
+++
@@ -1,8 +0,0 @@
-total_minutes = int(input("Enter the total number of minutes: "))
-
-
-hours = total_minutes // 60       
-minutes = total_minutes % 60      
-
-
-print(f"{total_minutes} minutes is {hours} hours and {minutes} minutes.")
--- rectangle.py
+++
@@ -1,7 +0,0 @@
-height=float(input("what is the height of the rectangle"))
-withh=float(input("what is the with or lengh of the rectangle"))
-
-aria = float
-
-aria = withh*height
-print(aria)
